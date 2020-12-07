188476f800d32f071ae82941629435c35dc4cb10
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-06 16:31:57 -0800

    Created HellohumanApplication class wit ah render Member method than will display text with or without user input

--- hellohuman/src/main/java/com/practice/hellohuman/HellohumanApplication.java
+++ hellohuman/src/main/java/com/practice/hellohuman/HellohumanApplication.java
@@ -2,12 +2,23 @@ package com.practice.hellohuman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HellohumanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HellohumanApplication.class, args);
+	}
+	@RequestMapping("/")
+	public String code1(@RequestParam(value="x", required=false) String test) {
+		if (test != null) {
+			return "Hello " + test + "! Welcome to the Grid!";
+		}
+		return "Hello Human! Welcome to the Grid!";
 	}
 
 }
